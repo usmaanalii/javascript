b80e49290f222ca342503cdfa6b5d51b9adf10fa
usyyy <usmaanalii@hotmail.co.uk>
2017-06-27 22:30:12 +0100

    Completed 2/3 (20 mins) of Webcam Fun

--- JavaScript30/19 - Webcam Fun/scripts.js
+++ JavaScript30/19 - Webcam Fun/scripts.js
@@ -3,3 +3,46 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        })
+        .catch(error => {
+            console.log('OH NO!', error);
+        });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+
+function takePhoto() {
+    // play the sound
+    snap.currentTime = 0;
+    snap.play();
+
+    // take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.textContent = 'Download Image';
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`; 
+    strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+
+video.addEventListener('canplay', paintToCanvas);
