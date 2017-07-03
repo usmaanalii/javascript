59427afe1275362584906644950235b816276ee4
usyyy <usmaanalii@hotmail.co.uk>
2017-07-03 15:30:12 +0100

    Completed mini project 2 - Link highlighter

--- JavaScript30/22 - Follow Along Link Highlighter/script.js
+++ JavaScript30/22 - Follow Along Link Highlighter/script.js
@@ -6,10 +6,16 @@ document.body.append(highlight);
 
 function highlightLink() {
     const linkCoords = this.getBoundingClientRect();
-    highlight.style.width = `${linkCoords.width}px`;
-    highlight.style.height = `${linkCoords.height}px`;
-    highlight.style.transform = `translate(${linkCoords.left}px, ${linkCoords.top}px)`;
-    console.log(linkCoords);
+    console.log('linkCoords', linkCoords);
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX,
+    }
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 }
 
 triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
