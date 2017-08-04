1438705519ea94a3cb0c3ad736186b01af4201a6
usyyy <usmaanalii@hotmail.co.uk>
2017-08-04 23:41:21 +0100

    Completed 12 - Code Quality with ESLint

--- ES6ForEveryone/12 - Code Quality with ESLint/bad-code.js
+++ ES6ForEveryone/12 - Code Quality with ESLint/bad-code.js
@@ -1,4 +1,4 @@
-var weather = new Promise((resolve) => {
+const weather = new Promise((resolve) => {
   setTimeout(() => {
     resolve({ temp: 29, conditions: 'Sunny with Clouds' });
   }, 2000);
@@ -12,7 +12,7 @@ const tweets = new Promise((resolve, reject) => {
 
 Promise
   .all([weather, tweets])
-  .then(responses => {
+  .then((responses) => {
     const [weatherInfo, tweetInfo] = responses;
     console.log(weatherInfo, tweetInfo);
   });
@@ -22,9 +22,7 @@ const streetCarsPromise = fetch('http://data.ratp.fr/api/datasets/1.0/search/?q=
 
 Promise
   .all([postsPromise, streetCarsPromise])
-  .then(responses => {
-    return Promise.all(responses.map(res => res.json()));
-  })
-  .then(responses => {
+  .then(responses => Promise.all(responses.map(res => res.json())))
+  .then((responses) => {
     console.log(responses);
   });
--- ES6ForEveryone/12 - Code Quality with ESLint/docs.md
+++ ES6ForEveryone/12 - Code Quality with ESLint/docs.md
@@ -1,5 +1,5 @@
 # Fake JavaScript Docs
 
 ```js
-var x = 100;
+var x = 10;
 ```
